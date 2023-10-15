82c0c7900f918716a6bb9dbe3a526118f78b0168
DastanDevv <dastanalmaz6eko1ch@gmail.com>
2023-10-15 15:13:15 +0600

    fitrs commit game

---
+++ lib/constants/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color grey = Color(0xffB4B4B4);
+  static const Color inActive = Color(0xff727573);
+  static const Color active = Color(0xff1BC11C);
+  static const Color red = Colors.red;
+  static const Color yellow = Color(0xffF09D12);
+  static const Color black = Color(0xff000000);
+}
